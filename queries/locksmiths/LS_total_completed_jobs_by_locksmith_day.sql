3f66fa6c9effcc98cc2ef4939c392feb27172867
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-15 13:22:55 +0100

    change filter in the query of locksmiths for completed jobs

--- queries/locksmiths/LS_total_completed_jobs_by_locksmith_day.sql
+++ queries/locksmiths/LS_total_completed_jobs_by_locksmith_day.sql
@@ -15,6 +15,14 @@ LEFT JOIN [dbo].[Policy_Financial] PF
 ON LD.ReportID = PF.ReportID
 WHERE LD.Selected = 1
 AND LS.LocksmithName LIKE ('WGTK%')
+AND LD.ReportID IN (
+	SELECT
+	DISTINCT(PD.ReportID)
+	FROM
+	[dbo].[Policy_Diary] PD
+	WHERE PD.Active = 0
+	AND CAST(PD.ClosedDate AS DATE) = CAST(GETDATE() AS DATE)
+)
 AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
 ) AS SB
 WHERE SB.NetCost IS NOT NULL
