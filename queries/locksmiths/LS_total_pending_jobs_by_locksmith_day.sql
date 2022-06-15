35bef755995daa17689e39153df9354958ab3220
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-15 13:37:46 +0100

    change queries for completed and pending jobs for locksmiths

--- queries/locksmiths/LS_total_pending_jobs_by_locksmith_day.sql
+++ queries/locksmiths/LS_total_pending_jobs_by_locksmith_day.sql
@@ -7,21 +7,24 @@ FROM
 SELECT
 LD.*,
 LS.LocksmithName,
-PD.ClosedDate,
 PF.NetCost
 FROM [dbo].[Policy_LocksmithDetails] LD
 LEFT JOIN [dbo].[Lookup_Locksmiths] LS
 ON LD.LocksmithID = LS.ID
-LEFT JOIN [dbo].[Policy_Diary] PD
-ON LD.ReportID = PD.ReportID
-LEFT JOIN [dbo].[Policy_Details] DE
-ON PD.ReportID = DE.ReportID
 LEFT JOIN [dbo].[Policy_Financial] PF
 ON LD.ReportID = PF.ReportID
 WHERE LD.Selected = 1
 AND LS.LocksmithName LIKE ('WGTK%')
-AND PD.Active = 1
-AND DE.StatusID <> 15
+AND LD.ReportID IN (
+	SELECT
+	DISTINCT(PD.ReportID)
+	FROM
+	[dbo].[Policy_Diary] PD
+	LEFT JOIN [dbo].[Policy_Details] DE
+	ON PD.ReportID = DE.ReportID
+	WHERE PD.Active = 1
+	AND DE.StatusID <> 15
+)
 AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
 ) AS SB
 WHERE SB.NetCost IS NULL
