635d1907bb67c4fe5b3edd0f8b7d67702c2f17fe
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-16 15:26:28 +0100

    Add a query for compare the selected locksmith vs the inovoice locksmith

---
+++ queries/locksmiths/LS_selected_vs_invoice_locksmiths.sql
@@ -0,0 +1,24 @@
+------------- Cases when the selected locksmith is not the same as the invoice's locksmith
+SELECT
+LD.ReportID,
+LS.LocksmithName,
+PF.RecipientName,
+PF.NetCost
+FROM [dbo].[Policy_LocksmithDetails] LD
+LEFT JOIN [dbo].[Lookup_Locksmiths] LS
+ON LD.LocksmithID = LS.ID
+LEFT JOIN [dbo].[Policy_Financial] PF
+ON LD.ReportID = PF.ReportID
+WHERE LD.Selected = 1
+AND LS.LocksmithName LIKE ('WGTK%')
+AND LD.ReportID IN (
+	SELECT
+	DISTINCT(PD.ReportID)
+	FROM
+	[dbo].[Policy_Diary] PD
+	WHERE PD.Active = 0
+	AND CAST(PD.ClosedDate AS DATE) = CAST(GETDATE() AS DATE)
+)
+AND LS.LocksmithName <> PF.RecipientName
+AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
+ORDER BY PF.NetCost DESC
